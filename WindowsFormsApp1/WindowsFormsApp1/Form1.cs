7b5cea0722353731fdfb962ed80668963fa05ff5
Chocolate-protein <138810014+Chocolate-protein@users.noreply.github.com>
2023-09-22 12:14:30 +0900

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "김영기";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Red;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Blue;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Black;
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "멜론") {
+                label2.Text = comboBox1.SelectedItem.ToString();
+                pictureBox1.ImageLocation = @"C:\Users\user\Desktop\WindowsFormsApp2\images\멜론.jpg";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "복숭아")
+            {
+                label2.Text = comboBox1.SelectedItem.ToString();
+                pictureBox1.ImageLocation = @"C:\Users\user\Desktop\WindowsFormsApp2\images\복숭아.jpg";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "망고")
+            {
+                label2.Text = comboBox1.SelectedItem.ToString();
+                pictureBox1.ImageLocation = @"C:\Users\user\Desktop\WindowsFormsApp2\images\망고.jpg";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "딸기")
+            {
+                label2.Text = comboBox1.SelectedItem.ToString();
+                pictureBox1.ImageLocation = @"C:\Users\user\Desktop\WindowsFormsApp2\images\딸기.jpg";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "바나나")
+            {
+                label2.Text = comboBox1.SelectedItem.ToString();
+                pictureBox1.ImageLocation = @"C:\Users\user\Desktop\WindowsFormsApp2\images\바나나.jpg";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                label2.Text = "멜론";
+                pictureBox1.ImageLocation = @"C:\Users\user\Desktop\WindowsFormsApp2\images\멜론.jpg";
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+                label2.Text = "복숭아";
+                pictureBox1.ImageLocation = @"C:\Users\user\Desktop\WindowsFormsApp2\images\복숭아.jpg";
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                label2.Text = "망고";
+                pictureBox1.ImageLocation = @"C:\Users\user\Desktop\WindowsFormsApp2\images\망고.jpg";
+            }
+        }
+    }
+}
